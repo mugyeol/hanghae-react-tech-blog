c82998f2a9794090e29916c78b749a080637e01d
mugyeol <htpadkorik@naver.com>
2022-12-09 17:48:25 +0900

    style : add font

--- src/shared/GlobalStyle.js
+++ src/shared/GlobalStyle.js
@@ -2,13 +2,19 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 
+@font-face {
+    font-family: 'NanumSquareNeo-Variable';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.0/NanumSquareNeo-Variable.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+}
   * {
     box-sizing: border-box;
     padding: 0;
     margin: 0;
     text-decoration: none;
     outline : none;
-    font-family: 'Noto Sans KR', sans-serif;
+    font-family: 'NanumSquareNeo-Variable';
   }
 
   body {
