783d6e835d10fef8fe81590570b1bc162f88e3c0
Suebin An <jejupalette@naver.com>
2022-12-10 13:39:09 +0900

    feat : 골격 레이아웃 추가

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  position: absolute;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  width: 100vw;
+  padding: 20px;
+`;
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  )
-}
+    <Container>
+      <div>© Mugyeol, suno0140, AnSuebin</div>
+    </Container>
+  );
+};
 
-export default Footer
+export default Footer;
