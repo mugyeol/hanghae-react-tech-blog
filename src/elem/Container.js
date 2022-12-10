c9e0391da04eb724807ecbf9c80670024f77ae07
mugyeol <htpadkorik@naver.com>
2022-12-10 15:44:38 +0900

    feat : add container element

---
+++ src/elem/Container.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Container
+
+const StContainer = styled.div`
+
+
+
+`
