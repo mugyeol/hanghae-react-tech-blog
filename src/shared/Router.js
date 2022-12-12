aaef833ddc9d4e0f8043b5746a235435243c3cfb
Suebin An <jejupalette@naver.com>
2022-12-12 17:48:34 +0900

    fix : addpostform 컴포넌트로 라우터 수정

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -2,19 +2,19 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "../pages/Main";
 import Detail from "../pages/Detail";
-import AddPostForm from "../features/post/AddPostForm";
+import Create from "../pages/Create";
 import Layout from "../components/Layout";
 const Router = () => {
   return (
     <>
       {/* <Layout> */}
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="/post/:id" element={<Detail />} />
-            <Route path="/form" element={<AddPostForm />} />
-          </Routes>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/post/:id" element={<Detail />} />
+          <Route path="/form" element={<Create />} />
+        </Routes>
+      </BrowserRouter>
       {/* </Layout> */}
     </>
   );
