3d3b77617bde69a3c515449644b4698ee3b36a49
mugyeol <htpadkorik@naver.com>
2022-12-12 14:55:39 +0900

    feat : 카테고리 분류 및 버튼 포커싱

--- src/features/post/PostList.js
+++ src/features/post/PostList.js
@@ -1,26 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Stack from "../../elem/Stack";
 import axios from "axios";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { useNavigate } from "react-router-dom";
-import { __getPosts } from "../../redux/modules/postSlice";
+import {
+  __getPosts,
+  __getPostsByCategory,
+} from "../../redux/modules/postSlice";
+import Button from "../../elem/Button";
 
 const PostList = () => {
   const dispatch = useDispatch();
   const { posts, error } = useSelector((state) => state.posts);
-  console.log(posts, "posts");
-  // const [postState, setPostState] = useState([]);
   const navigate = useNavigate();
-
-  // const fetchPosts = async () => {
-  //   const { data } = await axios.get("http://localhost:3001/data");
-  //   setPostState(data.posts); // 서버로부터 fetching한 데이터를 useState의 state로 set 합니다.
-  // };
 
   useEffect(() => {
     dispatch(__getPosts());
   }, [dispatch]);
+
+  const onClicCategorykHandler = (ctgr) => {
+    ctgr === "All"
+      ? dispatch(__getPosts())
+      : dispatch(__getPostsByCategory(ctgr));
+  };
 
   return (
     <Stack
@@ -29,14 +32,22 @@ const PostList = () => {
       direction="column"
       gap="40px"
     >
-      {posts.map((post) => (
-        <Card
+      <StCategory>
+        {posts.categories.map((category) => (
+          <Button border={category.isActive ? `3px solid var(--color-point1)` : null} key={category.id} onClick={() => onClicCategorykHandler(category.ctgr)}>
+            {category.ctgr}
+          </Button>
+        ))}
+      </StCategory>
+      {posts.list.map((post) => (
+        <Card key={post.pId} 
           onClick={() => {
             navigate(`/post/${post.pId}`);
           }}
         >
           <h1>{post.title}</h1>
-          <textarea value={post.content} readOnly rows={4}/>
+          <textarea value={post.content} readOnly rows={4} />
+          <Button>{post.category}</Button>
         </Card>
       ))}
     </Stack>
@@ -45,7 +56,7 @@ const PostList = () => {
 
 export default PostList;
 const Card = styled.div`
-  border: 1px solid blueviolet;
+  border: 1px solid var(--color-point2);
   width: 100%;
   height: 170px;
   padding: 15px;
@@ -63,4 +74,12 @@ const Card = styled.div`
     border-color: transparent;
     cursor: grab;
   }
+`;
+const StCategory = styled.div`
+  display: flex;
+  direction: row;
+  gap: 5px;
+  padding: 5px;
+  align-items: center;
+  justify-content: flex-start;
 `;
