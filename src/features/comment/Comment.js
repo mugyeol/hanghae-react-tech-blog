887425a238d06a1f3fc9fd404abcabe096d5d1b4
suno Choi <vlfxhd@naver.com>
2022-12-13 22:16:16 +0900

    feat: reply 댓글기능추가

--- src/features/comment/Comment.js
+++ src/features/comment/Comment.js
@@ -1,11 +1,91 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import Button from "../../elem/Button";
 
 const Comment = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [comments, setComments] = useState("");
 
-export default Comment
+  const [addComment, setAddComment] = useState({
+    useName: "",
+    comment: "",
+    isDone: false,
+  });
+
+  const onClickAddButton = (comment) => {
+    console.log(comment);
+    axios.post("http://localhost:3001/comments", comment);
+    setAddComment({
+      useName: "",
+      comment: "",
+      isDone: false,
+    });
+    setComments([...comments, comment]);
+  };
+
+  return (
+    <StComment>
+      <StUserName
+        type="text"
+        value={addComment.useName}
+        onChange={(event) => {
+          const { value } = event.target;
+          setAddComment({
+            ...addComment,
+            useName: value,
+          });
+        }}
+        placeholder="닉네임을 입력해주세요"
+      ></StUserName>
+      <StCommentInput
+        type="text"
+        value={addComment.comment}
+        onChange={(event) => {
+          const { value } = event.target;
+          setAddComment({
+            ...addComment,
+            comment: value,
+          });
+        }}
+        placeholder="댓글을 달아주세요"
+      ></StCommentInput>
+      <Button
+        type="button"
+        onClick={() => {
+          onClickAddButton(addComment);
+        }}
+      >
+        작성
+      </Button>
+    </StComment>
+  );
+};
+
+export default Comment;
+
+const StComment = styled.div`
+  display: flex;
+  align-items: center;
+  width: 90%;
+  height: 100px;
+  font-size: 20px;
+  margin: 15px 0;
+`;
+
+const StCommentInput = styled.input`
+  width: 68%;
+  padding: 10px 10px;
+  margin: 15px 10px 20px 5px;
+  box-sizing: border-box;
+  border: solid 2px #323232;
+  border-radius: 8px;
+`;
+
+const StUserName = styled.input`
+  width: 17%;
+  padding: 10px 10px;
+  margin: 15px 0 20px 30px;
+  box-sizing: border-box;
+  border: solid 2px #323232;
+  border-radius: 8px;
+`;
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,93 +1,49 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import CommentList from "../features/comment/CommentList";
+import Comment from "../features/comment/Comment";
 import axios from "axios";
+import { useParams } from "react-router-dom";
+import Profile from "../features/profile/Profile";
+import postSlice from "../redux/modules/postSlice";
 
 const Detail = () => {
-  // const [comment, setComment] = useState({
-  //   id: 0,
-  //   useName: "",
-  //   comment: "",
-  // });
-
-  const [comments, setComments] = useState("");
-  const fetchComments = async () => {
-    const { data } = await axios.get("http://localhost:3001/comments");
-    setComments(data);
-  };
-
-  // const [addName, setAddName] = useState({ comment: "" });
-  const [addComment, setAddComment] = useState({
-    useName: "",
-    comment: "",
+  const { param } = useParams();
+  const [post, setPost] = useState({
+    title: "",
+    content: "",
   });
-  const onClickAddButton = (comment) => {
-    console.log(comment);
-    axios.post("http://localhost:3001/comments", comment);
-    setAddComment({
-      useName: "",
-      comment: "",
-    });
-    setComments([...comments, comment]);
+
+  const fetchPost = async () => {
+    const { data } = await axios.get("http://localhost:3001/post");
+    const post = data.find((post) => post.id === parseInt(param));
+    setPost(post);
   };
 
   useEffect(() => {
-    fetchComments();
+    fetchPost();
   }, []);
 
   return (
     <Stcontainer>
-      <Category>카테고리</Category>
-      <Title>제목</Title>
-      <Stbody>글작성내용</Stbody>
-      <Stimage>작성자이미지, 이름, 깃허브</Stimage>
-      <StComment>
-        <StUserName
-          type="text"
-          value={addComment.useName}
-          onChange={(event) => {
-            const { value } = event.target;
-            setAddComment({
-              ...addComment,
-              useName: value,
-            });
-          }}
-          placeholder="닉네임을 입력해주세요"
-        ></StUserName>
-        <StCommentInput
-          type="text"
-          value={addComment.comment}
-          onChange={(event) => {
-            const { value } = event.target;
-            setAddComment({
-              ...addComment,
-              comment: value,
-            });
-          }}
-          placeholder="댓글을 달아주세요"
-        ></StCommentInput>
-        <Stbutton
-          type="button"
-          onClick={() => {
-            onClickAddButton(addComment);
-          }}
-        >
-          댓글작성
-        </Stbutton>
-      </StComment>
-      {comments &&
-        comments.map((comment) => (
-          <StComments key={comment.id}>
-            {comment.useName} - {comment.comment}
-            <UpdateButton>수정</UpdateButton>
-            <DeleteButton>삭제</DeleteButton>
-          </StComments>
-        ))}
+      <Title>{post.title}</Title>
+      {post.category}
+      <Stbody>{post.content}</Stbody>
+      <div>
+        <Profile />
+      </div>
+      <Comment />
+      <CommentList />
     </Stcontainer>
   );
 };
 
 export default Detail;
-const Stcontainer = styled.div``;
+const Stcontainer = styled.div`
+  margin: 0 auto;
+  width: 1000px;
+  min-width: 800px;
+`;
 const Category = styled.div`
   display: flex;
   align-items: center;
@@ -122,74 +78,4 @@ const Stbody = styled.div`
   width: 90%;
   height: 500px;
   margin: 15px 0 20px 30px;
-`;
-const Stimage = styled.div`
-  display: flex;
-  align-items: center;
-  width: 90%;
-  height: 100px;
-  font-size: 20px;
-  border: 2px solid #323232;
-  border-radius: 12px;
-  justify-content: center;
-  margin: 15px 0 20px 30px;
-`;
-const StComment = styled.div`
-  display: flex;
-  align-items: center;
-  width: 90%;
-  height: 100px;
-  font-size: 20px;
-  margin: 15px 0;
-`;
-const StComments = styled.div`
-  display: flex;
-  align-items: center;
-  width: 90%;
-  height: 50px;
-  font-size: 20px;
-  border: 2px solid #323232;
-  border-radius: 12px;
-  margin: 5px 0 5px 30px;
-  padding-left: 20px;
-`;
-const StCommentInput = styled.input`
-  width: 70%;
-  padding: 10px 10px;
-  margin: 15px 0 20px 5px;
-  box-sizing: border-box;
-  border: solid 2px #323232;
-  border-radius: 8px;
-`;
-const Stbutton = styled.button`
-  background-color: #323232;
-  color: white;
-  width: 150px;
-  padding: 10px 10px;
-  margin: 15px 0 20px 10px;
-`;
-
-const UpdateButton = styled.button`
-  background-color: #415c5a;
-  color: white;
-  width: 50px;
-  padding: 5px;
-  margin: 20px 0 20px 10px;
-`;
-
-const DeleteButton = styled.button`
-  background-color: #f6c69e;
-  color: black;
-  width: 50px;
-  padding: 5px;
-  margin: 20px 0 20px 10px;
-`;
-
-const StUserName = styled.input`
-  width: 20%;
-  padding: 10px 10px;
-  margin: 15px 0 20px 30px;
-  box-sizing: border-box;
-  border: solid 2px #323232;
-  border-radius: 8px;
 `;
